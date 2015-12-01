63b03ea21793020c1d80d2731c71fe6731334823
Yogesh <yogesh.thapar@gmail.com>
2015-12-01 16:37:28 +0530

    otstrap

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,3 +8,24 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+$(function() {
+
+  $('#alertMe').click(function(e){
+
+    e.preventDefault();
+
+    $('#successAlert').slideDown();
+
+  });
+
+  $('a.pop').click(function(e){
+
+    e.preventDefault();
+
+
+
+  });
+  $('a.pop').popover();
+  $('[rel="tooltip"]').tooltip();
+
+});
